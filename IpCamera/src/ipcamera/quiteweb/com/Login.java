f77eb692f24f41ba2386d22e581cd193a09d1d99
travelnet.dev <travelnet.dev@6a2b94db-88c6-de23-2d41-310451b4af0c>
2014-03-15 23:29:12 +0000

    Proxy test

--- IpCamera/src/ipcamera/quiteweb/com/Login.java
+++ IpCamera/src/ipcamera/quiteweb/com/Login.java
@@ -1,5 +1,15 @@
 package ipcamera.quiteweb.com;
 
-public class Login {
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
+public class Login implements OnClickListener {
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
